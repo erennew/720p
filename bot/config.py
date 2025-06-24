807ca401ba7f565bd662ae721317164c6fcfefe3
erennew <erenyeagermikasa84@gmail.com>
2025-06-24 21:20:23 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,18 +5,19 @@ class Config(object):
     SESSION_NAME = get_config("SESSION_NAME", "EncoderX") 
     # EncoderX_bot....
     # sucks Dude
-    APP_ID = int(get_config("APP_ID", "26387127"))
-    API_HASH = get_config("API_HASH", "19718ab7acd97d0f71ada2807ddfe47a")
+    APP_ID = int(get_config("APP_ID", "20793620"))
+    API_HASH = get_config("API_HASH", "a712d2b8486f26c4dee5127cc9ae0615")
     LOG_CHANNEL = get_config("LOG_CHANNEL", "logencoder")
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
-     # Get these values from my.telegram.org
-    AUTH_USERS = [5446367898]
+    AUTH_USERS = [1047253913]
+    USER_SESSION = get_config("USER_SESSION","BQAO83IAcjM8qkEXaikoZwkn8iWi94nr29TNTZpbjMI3KPlGAq-0CuA8idOxYur5HgYPIhMr4d5tka7WsRYAVM9CayPr9_8Clj3Brk_ye-xq81RpnizkaLyHLGkh83tW3AmWy3Mc_GhuvyIUblf0yaK5Xti2GGT2KezRqKA408kKIUHacBqfY4TCMOeA85ve0bMR5vKHxXh8MN09jsqD3-6wgBezxKnowXyF6KXXIxB4TQZP5PJh5OWp_yi_wX3e4gVnOWL2iE8LdI7UFKZ-0atI0gNGbi4Hbxg2GZgJMzMvJQkZMeifgj1R2UtZSRy0sp8uNl1sg8rccU1vbqvroFSACZB2EAAAAABAEDjEAA")  
+
 # array , simplest method was AUTH_USERS = [] ; AUTH_USERS.append(your telegram id) 🌹
     TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "8102040984:AAEQ9J8FFA_uneRT6cHbkoPMM7u5eItm-0I")
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
-    BOT_USERNAME = get_config("BOT_USERNAME", "Tjggursgjybot")
+    BOT_USERNAME = get_config("BOT_USERNAME", "Forwardmsgremoverbot")
     MAX_FILE_SIZE = 4194304000
     TG_MAX_FILE_SIZE = 4194304000
     FREE_USER_MAX_FILE_SIZE = 4194304000
@@ -28,7 +29,7 @@ class Config(object):
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # add config vars for the display progress
-    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "")
-    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "")
+    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "🟨")
+    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "⬛")
     LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
     SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
